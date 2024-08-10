fda05f94fd7f64c4b2c1bad376273117e419a597
Alien <lmllewellyn@gmail.com>
2024-08-10 11:51:15 -0400

    added relationships between models, added seeds file for post model

--- models/index.js
+++ models/index.js
@@ -2,4 +2,17 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
+Post.hasOne(User, {
+    foreignKey: 'post_user',
+    onDelete: 'CASCADE',
+});
+
+User.hasMany(Post, {
+    foreignKey: 'post_user',
+    onDelete: 'CASCADE',
+});
+
+Comment.belongsTo(User);
+Post.belongsTo(User);
+
 module.exports = { User, Post, Comment };
