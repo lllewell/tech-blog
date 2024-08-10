2c97cbdc9b75fe58d5c8ba21a9f0f4c478e29082
Alien <lmllewellyn@gmail.com>
2024-08-10 10:45:40 -0400

    added rough drafts of models

---
+++ models/Post.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_user: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        references: {
+            key: 'username',
+            model: 'user',
+        },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
+module.exports = Post;
