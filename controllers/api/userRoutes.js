0f20b11e5cee83fb698b799e75e49e52d07abe35
Alien <lmllewellyn@gmail.com>
2024-08-11 21:08:39 -0400

    added signup form and logic into login folder, added route in User.routes, imported style.css stylesheet

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -33,6 +33,26 @@ router.post('/login', async (req, res) => {
   }
 });
 
+router.post('/', async (req, res) => {
+  try {
+    const dbUserData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      console.log(dbUserData);
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 router.post('/logout', (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
